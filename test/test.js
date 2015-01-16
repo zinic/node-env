7cc3ef81de9679c564fd24b0f47b617e6ac64214
Phani Raj <phanirajuyn@gmail.com>
2015-01-15 19:17:27 -0800

    Added realistic tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 var http = require('http'),
     should = require('should'),
+    sysInfo = require('../lib/sysinfo.js'),
+    keyProcessor = require('../lib/keyprocessor.js'),
     hock = require('hock');
 
 describe('node-env/sdk/environment', function (done) {
@@ -10,17 +12,33 @@ describe('node-env/sdk/environment', function (done) {
   }),
 
   it("Can list system environment variables", function () {
+    var env = sysInfo.sysInfo();
+    should.exist(env);
+    env.length.should.be.equal(5);
   });
 
-  it("Can list ALS environment variables", function () {
+  it("Key processor leaves values unchanged", function () {
+    var result = keyProcessor.procKey('Name', 'VALUE');
+    result.should.be.equal('VALUE');
   });
 
-  it("Can list user environment variables", function () {
+  it("Key processor replaces \\r with an empty space", function () {
+    var result = keyProcessor.procKey('Name', 'VALUE\r');
+    result.should.be.equal('VALUE');
   });
 
-  it("Can list low privilege user environment variables", function () {
+  it("Key processor replaces \\n with a <br/> tag", function () {
+    var result = keyProcessor.procKey('Name', 'VALUE\n');
+    result.should.be.equal('VALUE<br/>');
   });
 
-  it("Cannot list high privilege user environment variables", function () {
+  it("Key processor generates a <span> tag for the path variable", function () {
+    var result = keyProcessor.procKey('path', 'VALUE');
+    result.should.not.be.equal('VALUE');
+  });
+
+  it("Key processor generates a <span> tag for the path variable", function () {
+    var result = keyProcessor.procKey('vmc_app_instance', '{"app_name" : "VALUE"}');
+    result.should.not.be.equal('VALUE');
   });
 });
